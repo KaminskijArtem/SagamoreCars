3a4fc6884dbc16885f0264270d8c91d3fac4fd75
Sagamore <kaminskijaa@gmail.com>
2020-05-21 23:39:27 +0300

    Init

---
+++ SagamoreCars/DemoService.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Threading;
+
+namespace SagamoreCars
+{
+    internal class DemoService : IDemoService
+    {
+        public void StartSetupDB()
+        {
+            var page = 0;
+            var url = @"https://cars.av.by/honda/civic/page/" + page;
+
+            while (true)
+            {
+                url = url.Substring(0, url.Length - (page++).ToString().Length) + page;
+                var isContinue = ParseAnnouncements(url);
+                if (!isContinue)
+                    break;
+            }
+        }
+
+        private bool ParseAnnouncements(string url)
+        {
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine(url);
+
+            var isContinue = true;
+
+            var web = new HtmlWeb();
+            var htmlDoc = web.Load(url);
+
+            var allAnnouncements = htmlDoc.DocumentNode
+                .SelectNodes("//div[@class = 'listing-wrap']/div[contains(@class, 'listing-item')]");
+
+            if (allAnnouncements.Count < 25)
+                isContinue = false;
+
+            foreach (var announcement in allAnnouncements)
+            {
+                var s = announcement
+                    .SelectSingleNode(".//div[@class = 'listing-item-image-in']/a")
+                    .GetAttributeValue("href", string.Empty);
+
+                Console.WriteLine(s);
+                Thread.Sleep(50);
+            }
+            return isContinue;
+        }
+    }
+}
---
+++ SagamoreCars/Program.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace SagamoreCars
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start of program");
+
+            RegisterServices();
+
+            var service = (IDemoService)_serviceProvider.GetService(typeof(IDemoService));
+            service.StartSetupDB();
+
+            Console.WriteLine("End of program");
+        }
+
+        private static void RegisterServices()
+        {
+            var collection = new ServiceCollection();
+            var builder = new ContainerBuilder();
+            builder.RegisterType<DemoService>().As<IDemoService>();
+            builder.Populate(collection);
+            var appContainer = builder.Build();
+            _serviceProvider = new AutofacServiceProvider(appContainer);
+        }
+    }
+}
